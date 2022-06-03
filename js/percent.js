85545bf9ac311c2193ca5f005cd13d8eac1e39f2
Keaton Martin <45956547+keatonmartin@users.noreply.github.com>
2022-06-02 23:20:56 -0400

    Update percent.js

--- js/percent.js
+++ js/percent.js
@@ -4,9 +4,7 @@ const endDate = new Date("2025-5-5");
 const startDateMs = startDate.getTime();
 const endDateMs = endDate.getTime();
 
-
-
 setInterval(function() {
   let percent = ( Date.now() - startDateMs ) / (endDateMs - startDateMs);
-  $("#percent").text(percent.toFixed(8) * 100 + "%");
+  $("#percent").text((percent * 100).toFixed(8) + "%");
 }, 50);
